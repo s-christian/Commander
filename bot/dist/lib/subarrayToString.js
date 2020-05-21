06b3d00f16818496f64455459eb6a0b82119910f
s-christian <61216427+s-christian@users.noreply.github.com>
2020-05-21 01:22:36 -0400

    Deleted old 'dist' directory

--- bot/dist/lib/subarrayToString.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function subarrayToString(arr, startIndex) {
-    return arr.slice(startIndex).join(" ");
-}
-exports.default = subarrayToString;
